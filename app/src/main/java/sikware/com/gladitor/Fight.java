551a91a5570ce7b6df2190a50602333474b4babf
apluth <apluth@crimson.ua.edu>
2016-05-10 16:27:49 -0500

    animated fight scene,decrease resolution and left originals in full resolution

--- app/src/main/java/sikware/com/gladitor/Fight.java
+++ app/src/main/java/sikware/com/gladitor/Fight.java
@@ -5,26 +5,42 @@ import android.graphics.drawable.StateListDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
-public class Fight extends AppCompatActivity {
+public class Fight extends AppCompatActivity implements Animation.AnimationListener  {
     Player p;
-
+    ImageView pic1;
+    AnimationDrawable attackAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fight);
         p=Global.p1;
-        ImageView p1=(ImageView)findViewById(R.id.fight);
-        StateListDrawable background=(StateListDrawable)p1.getBackground();
-        AnimationDrawable attackAnimation=(AnimationDrawable) background.getCurrent();
-        attackAnimation.setVisible(true,true);
-//        p1.setBackgroundResource(R.drawable.attack);
+        pic1=(ImageView)findViewById(R.id.fight);
+        pic1.setBackgroundResource(R.drawable.attack);
+        attackAnimation=(AnimationDrawable) pic1.getBackground();
+        //attackAnimation.start();
     }
-    public void fight(View view){
-        ImageView p1=(ImageView)findViewById(R.id.fight);
-        p1.setBackgroundResource(R.drawable.attack);
-        AnimationDrawable attackFrames=(AnimationDrawable)p1.getBackground();
-        attackFrames.start();
+    public void fight(View view) {
+        attackAnimation.start();
+       }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        pic1=(ImageView)findViewById(R.id.fight);
+        pic1.setBackgroundResource(R.drawable.attack);
+        attackAnimation=(AnimationDrawable) pic1.getBackground();
+        setContentView(R.layout.activity_fight);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
