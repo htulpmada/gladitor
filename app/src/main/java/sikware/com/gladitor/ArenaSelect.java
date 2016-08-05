29a1f9820c1af2c90dc8a03d1b185aec85467a01
Adam Pluth <Apluth@crimson.ua.edu>
2016-08-05 12:58:14 -0500

    compatability issues resolved for arenas

--- app/src/main/java/sikware/com/gladitor/ArenaSelect.java
+++ app/src/main/java/sikware/com/gladitor/ArenaSelect.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class ArenaSelect extends AppCompatActivity {
     RadioGroup rbg;
@@ -15,6 +16,7 @@ public class ArenaSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arena_select);
+        rbg = (RadioGroup)findViewById(R.id.radioGroup);
         rb=(RadioButton)findViewById(R.id.rb1);
         rb.setText(Global.loc.Arenas.get(0));
         rb=(RadioButton)findViewById(R.id.rb2);
@@ -25,11 +27,14 @@ public class ArenaSelect extends AppCompatActivity {
         rb.setText(Global.loc.Arenas.get(3));
         rb=(RadioButton)findViewById(R.id.rb5);
         rb.setText(Global.loc.Arenas.get(4));
+        TextView t =(TextView)findViewById(R.id.location);
+        t.setText(Global.loc.name);
+        t = (TextView)findViewById(R.id.badguy);
+        t.setText(Global.loc.badguys.get(0).name+"\n"+Global.loc.badguys.get(1).name+"\n"+Global.loc.badguys.get(2).name);
     }
     public void go(View view){
-        Intent fight = new Intent(this,Fight.class);
-        fight.putExtra("arena",rbg.getCheckedRadioButtonId());
+        Intent fight = new Intent(this, Fight.class);
+        fight.putExtra("arena", rbg.getCheckedRadioButtonId());
         startActivity(fight);
-        finish();
     }
 }
